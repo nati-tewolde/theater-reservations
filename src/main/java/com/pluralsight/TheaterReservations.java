78d14603bffa1c3b559a81b3a659cf225374755a
nati-tewolde <natisnewemail@gmail.com>
2025-09-30 10:47:11 -0400

    completed TheaterReservations solution

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,7 +1,40 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class TheaterReservations {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String name = input.nextLine();
+
+        System.out.print("What date will you be coming (MM/dd/yyyy): ");
+        String date = input.nextLine();
+
+        System.out.print("How many tickets would you like? ");
+        int tickets = input.nextInt();
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate formattedDate = LocalDate.parse(date, dateFormatter);
+
+        int atSpace = name.indexOf(' ');
+        String firstName = name.substring(0, atSpace);
+        String lastName = name.substring(atSpace + 1);
+
+        String ticketName = "";
+        if(tickets <= 1){
+            ticketName = "ticket";
+        } else {
+            ticketName = "tickets";
+        }
+
+
+        System.out.println(tickets + " " + ticketName + " reserved for " + formattedDate + " under " + lastName + ", " + firstName);
+
 
     }
 }
